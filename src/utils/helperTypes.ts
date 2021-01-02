5d21e9c81c0c2b9ee027f054502b84450ba9b8f2
Victor Alvarenga <info@victoralvarenga.com>
2021-01-02 12:25:55 -0300

    Patch: esLint Fix on line 31

--- src/utils/helperTypes.ts
+++ src/utils/helperTypes.ts
@@ -28,7 +28,7 @@ export type SetDifference<A, B> = A extends B ? never : A;
 
 export type SetComplement<A, A1 extends A> = SetDifference<A, A1>;
 
-export type Subtract<T extends T1, T1 extends object> = Pick<
+export type Subtract<T extends T1, T1 extends Record<string, unknown>> = Pick<
   T,
   SetComplement<keyof T, keyof T1>
 >;
